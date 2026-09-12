facb582d30461b17875cf18ad20c53518152fe0d
AhmadAbufarha77 <abufarhaahmad5@gmail.com>
2026-09-12 11:39:28 +0300

    Initial project

---
+++ app/src/main/java/birzeit/edu/project/Profile.java
@@ -0,0 +1,30 @@
+package birzeit.edu.project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+public class Profile extends Fragment {
+
+    public Profile() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Button btnEditProfile = view.findViewById(R.id.btnEditProfile);
+        btnEditProfile.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), EditProfileActivity.class);
+            startActivity(intent);
+        });
+
+        return view;
+    }
+}
---
+++ app/src/main/java/birzeit/edu/project/SignUpActivity.java
@@ -0,0 +1,36 @@
+package birzeit.edu.project;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.widget.ImageButton;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.signup);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ImageView btnBack = findViewById(R.id.btnBack);
+        TextView tvSignIn = findViewById(R.id.tvToSignIn);
+
+        btnBack.setOnClickListener(v -> finish());
+        tvSignIn.setOnClickListener(v -> finish());
+    }
+}
