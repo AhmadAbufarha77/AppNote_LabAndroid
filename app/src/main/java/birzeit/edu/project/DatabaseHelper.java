be9f7a88ce0d9413a16cc9d901c1cbdb2d1561b1
AhmadAbufarha77 <abufarhaahmad5@gmail.com>
2026-09-12 15:11:45 +0300

    Update project SignUp Done

---
+++ app/src/main/java/birzeit/edu/project/DatabaseHelper.java
@@ -0,0 +1,56 @@
+package birzeit.edu.project;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version)
+    { super(context, name, factory, version); }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(
+                "CREATE TABLE USERS(" +
+                        "EMAIL TEXT PRIMARY KEY, " +
+                        "FIRST_NAME TEXT, " +
+                        "LAST_NAME TEXT, " +
+                        "PASSWORD TEXT)"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public boolean emailExists(String email) {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(
+                "SELECT EMAIL FROM USERS WHERE EMAIL = ?",
+                new String[]{email}
+        );
+
+        boolean exists = cursor.getCount() > 0;
+
+        cursor.close();
+
+        return exists;
+    }
+
+    public void insertUser(User user) {
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("EMAIL", user.getEmail());
+        contentValues.put("FIRST_NAME", user.getFirstName());
+        contentValues.put("LAST_NAME", user.getLastName());
+        contentValues.put("PASSWORD", user.getPassword());
+        sqLiteDatabase.insert("USERS", null, contentValues);
+    }
+
+}
--- app/src/main/java/birzeit/edu/project/SignUpActivity.java
+++ app/src/main/java/birzeit/edu/project/SignUpActivity.java
@@ -2,6 +2,10 @@ package birzeit.edu.project;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,9 +17,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SignUpActivity extends AppCompatActivity {
-//ahmad and maen
+    DatabaseHelper databaseHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,11 +34,147 @@ public class SignUpActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
+        databaseHelper = new DatabaseHelper(
+                this,
+                "NoteApp.db",
+                null,
+                1
+        );
         ImageView btnBack = findViewById(R.id.btnBack);
         TextView tvSignIn = findViewById(R.id.tvToSignIn);
 
         btnBack.setOnClickListener(v -> finish());
         tvSignIn.setOnClickListener(v -> finish());
+        //id
+        TextInputLayout tilEmail = (TextInputLayout) findViewById(R.id.tilSignUpEmail);
+        TextInputEditText etEmail =(TextInputEditText) findViewById(R.id.etSignUpEmail);
+
+        TextInputLayout tilFirstName = (TextInputLayout) findViewById(R.id.tilSignUpFirstName);
+        TextInputEditText etFirstName =(TextInputEditText) findViewById(R.id.etSignUpFirstName);
+
+        TextInputLayout tilLastName = (TextInputLayout) findViewById(R.id.tilSignUpLastName);
+        TextInputEditText etLastName =(TextInputEditText) findViewById(R.id.etSignUpLastName);
+
+        TextInputLayout tilPassword = (TextInputLayout) findViewById(R.id.tilSignUpPassword);
+        TextInputEditText etPassword =(TextInputEditText) findViewById(R.id.etSignUpPassword);
+
+        TextInputLayout tilConfirmPassword = (TextInputLayout) findViewById(R.id.tilConfirmPassword);
+        TextInputEditText etConfirmPassword =(TextInputEditText) findViewById(R.id.etConfirmPassword);
+
+        Button btnCreatAcc = (Button) findViewById(R.id.btnCreatAccount);
+
+        clearErrorOnTyping(etEmail, tilEmail);
+        clearErrorOnTyping(etFirstName, tilFirstName);
+        clearErrorOnTyping(etLastName, tilLastName);
+        clearErrorOnTyping(etPassword, tilPassword);
+        clearErrorOnTyping(etConfirmPassword, tilConfirmPassword);
+
+        btnCreatAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean valid=true;
+                if(!isValidEmail(etEmail.getText().toString())){
+                    tilEmail.setError("Invalid email");
+                    valid = false;
+                } else {tilEmail.setError(null);}
+                if (!isValidName(etFirstName.getText().toString())) {
+                    tilFirstName.setError("First name must be 3 to 10 characters");
+                    valid = false;
+                } else {tilFirstName.setError(null);}
+                if (!isValidName(etLastName.getText().toString())) {
+                    tilLastName.setError("Last name must be 3 to 10 characters");
+                    valid = false;
+                }else {tilLastName.setError(null);}
+                if (!isValidPassword(etPassword.getText().toString())) {
+                    tilPassword.setError("Password must be 6-12 characters and contain uppercase, lowercase and number");
+                    valid = false;
+                }else {tilPassword.setError(null);}
+                if (!etConfirmPassword.getText().toString().equals(etPassword.getText().toString())) {
+                    tilConfirmPassword.setError("Passwords do not match");
+                    valid = false;
+                } else {tilConfirmPassword.setError(null);}
+
+                if(!valid){return;}
+
+                if(databaseHelper.emailExists(etEmail.getText().toString())){
+                    tilEmail.setError("Email already registered");
+                }
+                else {
+                    User user = new User();
+                    user.setEmail(etEmail.getText().toString());
+                    user.setFirstName(etFirstName.getText().toString());
+                    user.setLastName(etLastName.getText().toString());
+                    user.setPassword(etPassword.getText().toString());
+                    databaseHelper.insertUser(user);
+
+                    Toast.makeText(
+                            SignUpActivity.this,
+                            "Account created successfully",
+                            Toast.LENGTH_SHORT
+                    ).show();
+
+                    finish();
+                }
+
+
+            }
+        });
+
+
     }
+
+    private boolean isValidEmail(String email) {
+            return email.matches("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
+    }
+
+    private boolean isValidName(String name){
+        return name.length() >= 3 && name.length() <= 10;
+    }
+
+    private boolean isValidPassword(String password) {
+        if (password.length() < 6 || password.length() > 12) {
+            return false;
+        }
+
+        boolean hasUpper = false;
+        boolean hasLower = false;
+        boolean hasNumber = false;
+
+        for (int i = 0; i < password.length(); i++) {
+
+            char c = password.charAt(i);
+
+            if (Character.isUpperCase(c)) {
+                hasUpper = true;
+            }
+
+            if (Character.isLowerCase(c)) {
+                hasLower = true;
+            }
+
+            if (Character.isDigit(c)) {
+                hasNumber = true;
+            }
+        }
+
+        return hasUpper && hasLower && hasNumber;
+    }
+
+    private void clearErrorOnTyping(TextInputEditText editText, TextInputLayout layout) {
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                layout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
 }
