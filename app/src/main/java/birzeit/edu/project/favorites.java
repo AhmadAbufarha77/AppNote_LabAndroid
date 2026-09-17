ddeab69f1198875714bec7f2c9163f7d50450130
AhmadAbufarha77 <abufarhaahmad5@gmail.com>
2026-09-17 21:26:03 +0300

    complete project

--- app/src/main/java/birzeit/edu/project/favorites.java
+++ app/src/main/java/birzeit/edu/project/favorites.java
@@ -116,7 +116,13 @@ public class favorites extends Fragment {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent =
+                        new Intent(
+                                requireContext(),
+                                AddNoteActivity.class
+                        );
 
+                startActivity(intent);
             }
         });
 
